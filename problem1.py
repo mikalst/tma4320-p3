9f8899a94defc53b2ceb2e01d12cf52e745ad719
oyvinau <oyvind.auestad@hotmail.com>
2017-03-29 14:49:08 +0200

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -9,30 +9,19 @@ STEP_ARRAY = np.linspace(0, TOTAL_TIME, STEPS)
 DT = TOTAL_TIME / STEPS
 
 
-def v_w(x_vec, dt):
+def v_w(x_vec, t, dt):
     T = 24*3600
     a = 2*np.pi/T * dt
 
     return [-a*x_vec[1], a*x_vec[0]]
 
 
-def get_global_error(number_of_steps):
-
-    position_vectors = np.zeros((number_of_steps, 2), dtype=float)
-    position_vectors[0] = [0.0, L]
-
-    dt = TOTAL_TIME / number_of_steps
-
-    for step in range(1, number_of_steps):
-        current_pos_vec = position_vectors[(step-1), :]
-
-        vel_vec = v_w(current_pos_vec, dt)
-
-        next_pos_vec = current_pos_vec + vel_vec
-
-        position_vectors[step] = next_pos_vec
-
-    return position_vectors[number_of_steps - 1, 1] - L
+def get_global_error(h): # h = dt
+    t_values = [t for t in range(0, 24 * 3600, h)]
+    X = [100, 0]
+    for t in t_values:
+        X += v_w(X, t, h)
+    return np.sqrt((X[0] - 100)**2 + X[1]**2)
 
 """
 num_steps_array = np.linspace(100, 3000, 50, dtype=int)
@@ -52,7 +41,9 @@ plt.show()
 
 error = 1E4
 
-while
+# Snakte me om at me hadde mange for-løkker i koden i går? Fant ut at det e mulig å gjør som dette :-)
+h_values = [h for h in rage(1728, 0, -1)]
+global_error_values = [get_global_error(h) for h in h_values]
 
 
 
